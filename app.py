d7c3c870838c24166a93ab6cd0a7054bc322f8d8
Antti Vuorenmaa <anttivuorenmaa@Anttis-Mac-mini.local>
2021-09-12 23:24:51 +0300

    ver 0.3 cleaned code with pylint

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+'''the main app'''
+from os import getenv
 from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
@@ -9,46 +11,48 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
+    '''all topic areas, so far in the root'''
     result = db.session.execute("SELECT id , name FROM topicareas")
     topicareas = result.fetchall()
-    return render_template("index.html", count=len(topicareas), topicareas=topicareas) 
+    return render_template("index.html", count=len(topicareas), topicareas=topicareas)
 
 @app.route("/newtopicarea")
 def newtopicarea():
+    '''Path for creating new topic areas (admin only'''
     return render_template("newtopicarea.html")
 
-@app.route("/<int:id>/newtopic") 
-def newtopic(id):  
-    return render_template("newtopic.html",topicareaid=id)    
-
-@app.route("/addnewtopicarea", methods=["POST"]) #TÄMÄ VAIN ADMINILLE: AIHEPIIRIEN LISÄÄMINEN
+@app.route("/addnewtopicarea", methods=["POST"])
 def addnewtopicarea():
+    '''Adds new topic areas'''
     content = request.form["content"]
     sql = "INSERT INTO topicareas (name) VALUES (:content)"
     db.session.execute(sql, {"content":content})
     db.session.commit()
     return redirect("/")
 
-@app.route("/<int:id>/addnewtopic", methods=["POST"])                 # UUDEN KESKUSTELUN LISÄÄMINEN
-def addnewtopic(id):
+@app.route("/<int:id>/newtopic")
+def newtopic(topicarea_id):
+    '''new topics get'''
+    return render_template("newtopic.html",topicareaid=topicarea_id)
+
+@app.route("/<int:id>/addnewtopic", methods=["POST"])
+def addnewtopic(topicarea_id):
+    '''add new topics'''
     content = request.form["content"]
-    topicarea_id=id
     sql = "INSERT INTO topics (topicarea_id,name) VALUES (:topicarea_id,:content)"
     db.session.execute(sql, {"content":content,"topicarea_id":topicarea_id})
     db.session.commit()
     return redirect("/"+str(id))
 
 
-@app.route("/<int:id>") #KESKUSTELUKETJUT GET
-def topics(id):
-    sql = "SELECT id , name FROM topics WHERE topicarea_id=:id "  
-    result = db.session.execute(sql, {"id":id})
+@app.route("/<int:id>")
+def topics(topicarea_id):
+    '''topics get'''
+    sql = "SELECT id , name FROM topics WHERE topicarea_id=:id "
+    result = db.session.execute(sql, {"id":topicarea_id})
     topics = result.fetchall()
-    sql = "SELECT name FROM topicareas WHERE id=:id "  
-    result = db.session.execute(sql, {"id":id})
+    sql = "SELECT name FROM topicareas WHERE id=:id "
+    result = db.session.execute(sql, {"id":topicarea_id})
     topicarea= result.fetchone()[0]
     print(topicarea)
-    return render_template("topics.html", count=len(topics), topics=topics, topicareaid=id, topicareaname=topicarea )    
-
-
-
+    return render_template("topics.html", count=len(topics), topics=topics, topicareaid=id, topicareaname=topicarea )
