d63a3db1c4da171b8dc3fc80a4fa1a5cc9ca0113
Antti Vuorenmaa <anttivuorenmaa@Anttis-Mac-mini.local>
2021-09-13 15:22:39 +0300

    ver 5.0 cleaned code with pylint, function for posting new topic

--- app.py
+++ app.py
@@ -30,38 +30,59 @@ def addnewtopicarea():
     db.session.commit()
     return redirect("/")
 
-@app.route("/<int:id>/newtopic")
-def newtopic(id):
+@app.route("/<int:topicrea_id>/newtopic")
+def newtopic(topicrea_id):
     '''new topics get'''
-    return render_template("newtopic.html",topicareaid=id)
+    return render_template("newtopic.html",topicareaid=topicrea_id)
 
-@app.route("/<int:id>/addnewtopic", methods=["POST"])
-def addnewtopic(id):
-    '''add new topics'''
-    content = request.form["content"]
-    sql = "INSERT INTO topics (topicarea_id,name) VALUES (:topicarea_id,:content)"
-    db.session.execute(sql, {"content":content,"topicarea_id":id})
+@app.route("/<int:topicrea_id>/addnewtopic", methods=["POST"])
+def addnewtopic(topicrea_id):
+    '''adds new topic, also adds the first message to the topic chain'''
+    title = request.form["title"]
+    sql = "INSERT INTO topics (topicarea_id,name) VALUES (:topicarea_id,:title) returning id"
+    result=db.session.execute(sql, {"title":title,"topicarea_id":topicrea_id})
+    topic_id = result.fetchone()[0]
+    print(topic_id)
     db.session.commit()
-    return redirect("/"+str(id))
+    content = request.form["content"]#tämä laitetaan MENEMÄÄN ekaks messageksi TARVITAAN TOPIC ID
+    sql = "INSERT INTO messages (topics_id,content) VALUES (:topic_id,:content)"
+    db.session.execute(sql, {"content":content,"topic_id":topic_id})
+    db.session.commit()
+    return redirect("/"+str(topicrea_id)+"/"+str(topic_id))
 
-@app.route("/<int:id>")
-def topics(id):
+@app.route("/<int:topicrea_id>")
+def topics(topicrea_id):
     '''topics get'''
     sql = "SELECT id , name FROM topics WHERE topicarea_id=:id "
-    result = db.session.execute(sql, {"id":id})
-    topics = result.fetchall()
+    result = db.session.execute(sql, {"id":topicrea_id})
+    selectedtopics = result.fetchall()
     sql = "SELECT name FROM topicareas WHERE id=:id "
-    result = db.session.execute(sql, {"id":id})
+    result = db.session.execute(sql, {"id":topicrea_id})
     topicarea= result.fetchone()[0]
-    return render_template("topics.html", count=len(topics), topics=topics, topicareaid=id, topicareaname=topicarea )
+    return render_template("topics.html", count=len(topics), topics=selectedtopics,
+    topicareaid=topicrea_id, topicareaname=topicarea )
 
-@app.route("/<int:topicarea_id>/<int:topic_id>")  
-def messages(topicarea_id,topic_id):
+@app.route("/<int:topicarea_id>/<int:topic_id>")
+def messages(topic_id):
     '''messages get'''
     sql = "SELECT id , content FROM messages WHERE topics_id=:topic_id"
     result = db.session.execute(sql, {"topic_id":topic_id})
-    messages= result.fetchall()
+    selectedmessages= result.fetchall()
     sql = "SELECT name FROM topics WHERE id=:topic_id "
     result = db.session.execute(sql, {"topic_id":topic_id})
     topicname= result.fetchone()[0]
-    return render_template("topic.html", messages=messages, topicname=topicname)
+    return render_template("topic.html", messages=selectedmessages, topicname=topicname)
+
+@app.route("/<int:topicarea_id>/<int:topic_id>/addnewmessage", methods=["POST"])
+def addnewmessage(topicarea_id,topic_id):
+    '''adds new message as a reply to the topic'''
+    content = request.form["content"]#tämä laitetaan ekaks messageksi
+ #   sql = "INSERT INTO topics (topicarea_id,name) VALUES (:topicarea_id,:title) RETURNING id"
+ #   result=db.session.execute(sql, {"title":title,"topicarea_id":topicarea_id})
+ #   topic_id = result.fetchone()[0]
+ #   db.session.commit()
+    print(content)
+    #sql = "INSERT INTO messages (topics_id,content) VALUES (:topicarea_id,:title)"
+    #db.session.execute(sql, {"content":content,"topics_id":})
+ #   db.session.commit()
+    return redirect("/"+str(topicarea_id)+"/"+str(topic_id))
