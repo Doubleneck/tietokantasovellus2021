a3bf2d5facb0f7f4ad49fc503677d60c575fec2d
Antti Vuorenmaa <anttivuorenmaa@Anttis-Mac-mini.local>
2021-09-22 12:51:56 +0300

    function for and query for counting messages

--- messages.py
+++ messages.py
@@ -5,7 +5,13 @@ def get_topicareas():
     '''all topic areas, starting page'''
     result = db.session.execute("SELECT id , name FROM topicareas")
     topicareas = result.fetchall()
-    return topicareas
+    list=[]
+    for t in topicareas:
+        
+        count_messages(t[0])
+        list.append((t[0],t[1],count_messages(t[0])))
+    print(list)    
+    return list
 
 def add_newtopicarea(topicarea_name):
     sql = "INSERT INTO topicareas (name) VALUES (:name)"
@@ -24,7 +30,6 @@ def count_chains(topicarea_id):
     count = result.fetchone()[0]
     return count
 
-
 def get_topics(topicarea_id):
      sql = "SELECT id , name FROM topics WHERE topicarea_id=:id "
      result = db.session.execute(sql, {"id":topicarea_id})
@@ -37,7 +42,7 @@ def add_newtopic (topicarea_id,user_id,topic_name,message_content):
     result=db.session.execute(sql, {"name":topic_name,"topicarea_id":topicarea_id,"user_id":user_id})
     topic_id = result.fetchone()[0]    
     db.session.commit()
-    sql2 = "INSERT INTO messages (topics_id, content, created_at,user_id) VALUES (:topic_id,:content,NOW(),:user_id)"
+    sql2 = "INSERT INTO messages (topics_id, content, created_at,user_id,visible) VALUES (:topic_id,:content,NOW(),:user_id,TRUE)"
     db.session.execute(sql2, {"topic_id":topic_id,"content":message_content,"user_id":user_id})
     db.session.commit()
     return topic_id 
@@ -57,12 +62,22 @@ def view_messages(topicarea_id,topic_id):#views VISIBLE=TRUE messages so far
     return selected_messages
 
 def count_visiblemessages(topic_id):
-    
     sql = "SELECT COUNT(*) FROM messages WHERE topics_id=:topic_id and visible=TRUE"
     result = db.session.execute(sql, {"topic_id":topic_id})
     count = result.fetchone()[0]
     return count
 
+
+
+def count_messages(topicarea_id):
+#    sql = "SELECT COUNT(*) FROM messages WHERE topics_id=:topic_id and visible=TRUE"
+#    result = db.session.execute(sql, {"topic_id":topic_id})
+#    count = result.fetchone()[0]
+#    return countdef get_list():count(*)
+    sql = "SELECT count(*) FROM messages M, topics T, topicareas A WHERE M.topics_id=T.id and T.topicarea_id=A.id  and A.id=:topicarea_id and M.visible=TRUE"  
+    #sql = "SELECT count(*) FROM messages M, Topics T WHERE M.topic_id=T.id and T.id=:topic_id"  
+    result = db.session.execute(sql, {"topicarea_id":topicarea_id})
+    return result.fetchone()[0]
 
 def new_message(topicarea_id,topic_id,content,user_id):
     sql = "INSERT INTO messages (topics_id, content, created_at,user_id,visible) VALUES (:topic_id,:content,NOW(),:user_id,TRUE)"
