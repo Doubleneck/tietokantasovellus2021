16d522bd2f6c9ed9b1f5a873fbbc5a35dfda5b44
“Doubleneck” <antti.vuorenmaa@kolumbus.fi>
2021-09-22 18:14:51 +0300

    Add Heroku config and schema.sql

---
+++ schema.sql
@@ -0,0 +1,17 @@
+
+ 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+ 
+DROP TABLE IF EXISTS visitors;
+CREATE TABLE visitors (id SERIAL PRIMARY KEY, moment TIMESTAMP);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (id SERIAL PRIMARY KEY, topic_id INTEGER REFERENCES topics, content TEXT, created_at TIMESTAMP, user_id INTEGER REFERENCES users, visible BOOLEAN);
+
+DROP TABLE IF EXISTS topicareas;
+CREATE TABLE topicareas (id SERIAL PRIMARY KEY, name TEXT, visible BOOLEAN);
+
+DROP TABLE IF EXISTS topics;
+CREATE TABLE topics (id SERIAL PRIMARY KEY, topicarea_id INTEGER REFERENCES topicareas, name TEXT, user_id REFERENCES users, visible BOOLEAN);
+
