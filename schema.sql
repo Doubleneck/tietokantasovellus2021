a87af6acf2d0aa171f9e41b01937a474ff8c563e
Antti Vuorenmaa <anttivuorenmaa@Anttis-Mac-mini.local>
2021-09-22 18:38:22 +0300

    Heroku add

--- schema.sql
+++ schema.sql
@@ -6,12 +6,12 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 DROP TABLE IF EXISTS visitors;
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, moment TIMESTAMP);
 
-DROP TABLE IF EXISTS messages;
-CREATE TABLE messages (id SERIAL PRIMARY KEY, topic_id INTEGER REFERENCES topics, content TEXT, created_at TIMESTAMP, user_id INTEGER REFERENCES users, visible BOOLEAN);
-
 DROP TABLE IF EXISTS topicareas;
 CREATE TABLE topicareas (id SERIAL PRIMARY KEY, name TEXT, visible BOOLEAN);
 
 DROP TABLE IF EXISTS topics;
-CREATE TABLE topics (id SERIAL PRIMARY KEY, topicarea_id INTEGER REFERENCES topicareas, name TEXT, user_id REFERENCES users, visible BOOLEAN);
+CREATE TABLE topics (id SERIAL PRIMARY KEY, topicarea_id INTEGER REFERENCES topicareas, name TEXT, user_id INTEGER REFERENCES users, visible BOOLEAN);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (id SERIAL PRIMARY KEY, topic_id INTEGER REFERENCES topics, content TEXT, created_at TIMESTAMP, user_id INTEGER REFERENCES users, visible BOOLEAN);
 
