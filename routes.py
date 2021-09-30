3eaed07177784369afd2e1ceef63f47a2f225dfe
“Doubleneck” <antti.vuorenmaa@kolumbus.fi>
2021-09-30 15:52:43 +0300

    add user/admin can delete topic

--- routes.py
+++ routes.py
@@ -9,8 +9,11 @@ def index():
         logged_user = users.username()
         visitscounter = visits.get_counter()
         topic_areas = messages.get_topicareas()
+        secret_topicareas=[]
+        if users.is_admin() or users.is_puser:
+            secret_topicareas = messages.get_secrettopicareas()
         return render_template("index.html", counter=visitscounter, logged=logged_user, 
-        count=len(topic_areas), topicareas=topic_areas)
+        count=len(topic_areas), topicareas=topic_areas, secretareas=secret_topicareas)
     except:
         return render_template("index.html")
 
@@ -100,14 +103,16 @@ def newsecret_area():
 def view_topics(topicarea_id):
     '''topics get'''
     try:
+        print("TÄÄLLÄ")
         selectedtopics=messages.get_topics(topicarea_id)
         topicarea=messages.get_topicareaname(topicarea_id)
         return render_template ("topics.html", count=len(selectedtopics), topics=selectedtopics,
                                 topicareaid=topicarea_id, topicareaname=topicarea)
-    except:
-        return "Not allowed"      
 
-                                 
+    except:
+        
+        return "Not allowed"      
+                                
 @app.route("/<int:topicarea_id>/newtopic", methods=["GET","POST"])
 def add_newtopic(topicarea_id):
     '''new topic = title for chain of messages'''
@@ -127,12 +132,13 @@ def add_newtopic(topicarea_id):
 def view_messages(topicarea_id,topic_id):
     '''messages get'''
     try:
+        topic_user_id=messages.get_topicowner(topic_id)
         selected_messages = messages.view_messages(topic_id)        
         topic_name = messages.get_topicname(topic_id)
         return render_template("topic.html", messages=selected_messages, topicareaid=topicarea_id,
-                               topicid=topic_id, topicname=topic_name)
+                               topicid=topic_id, topicname=topic_name, topicuserid=topic_user_id)
     except:
-        print("TÄÄLLÄ")
+        
         return "Not allowed"   
 
 @app.route("/<int:topicarea_id>/<int:topic_id>/addnewmessage", methods=["POST"])
@@ -145,6 +151,16 @@ def add_newmessage(topicarea_id,topic_id):
         return redirect("/"+str(topicarea_id)+"/"+str(topic_id))
     except:
         return "Not allowed"       
+
+@app.route("/<int:topicarea_id>/<int:topic_id>/delete", methods=["POST"])
+def delete_topic(topicarea_id,topic_id):
+    '''deletes topic (owner or admin)'''    
+    #messages.is_topicowner(topic_id) or users.is_admin():
+    messages.delete_messages(topic_id)
+    messages.delete_topic(topic_id)        
+    return redirect("/"+str(topicarea_id))
+    
+           
 
 @app.route("/<int:topicarea_id>/<int:topic_id>/<int:message_id>", methods=["POST"])
 def delete_message (topicarea_id,topic_id,message_id):
