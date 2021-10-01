f5f8ca085995449c0b63f56d4ca3d88911278e03
Antti Vuorenmaa <anttivuorenmaa@Anttis-Mac-mini.local>
2021-10-01 14:35:11 +0300

    validations and flash() for messages, topics, message & topic edit functions

--- utils.py
+++ utils.py
@@ -9,3 +9,8 @@ def validate_password(content):
     if re.match("^[a-zA-Z0-9]+$", content) and len(content)>=6:
         return True  
             
+def validate_message(content):
+    return len(content)>=3 and len(content)<=200         
+
+def validate_topic(content):
+    return len(content)>=10 and len(content)<=70  
